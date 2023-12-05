ec0122024bdebd9f96ead3cb8b1b7fef57d1284b
jonanv <jonan-vargas23@hotmail.com>
2023-12-04 22:04:10 -0500

    :sparkles: (challenge 04): Add challenge 04 of codember 2023

---
+++ 2023/challenge04/index.js
@@ -0,0 +1,33 @@
+import fs from 'node:fs';
+
+const data = fs.readFileSync('2023/challenge04/files_quarantine.txt', 'utf8');
+const arrayChecksum = data.split('\n');
+
+const realFiles = [];
+
+arrayChecksum.forEach(element => {
+    if (verifiedFiles(element)) realFiles.push(element);
+});
+console.log(realFiles);
+console.log('archivo real #33: ', realFiles[32]);
+
+function verifiedFiles(elementChecksum) {
+    let [chain, checksum] = elementChecksum.split('-');
+    let file = {};
+    let arrayChain = [...chain];
+
+    // console.log(arrayChain);
+    arrayChain.forEach((element) => {
+        (file[element] != undefined) 
+            ? file[element] += 1 
+            : file[element] = 1;
+    });
+    // console.log(file);
+    let fileChecksum = '';
+    
+    arrayChain.forEach((element) => {
+        if (file[element] === 1) fileChecksum += element;
+    });
+    // console.log(fileChecksum);
+    return fileChecksum === checksum;
+}

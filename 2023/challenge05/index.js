605ae46512a81b0c44df61f8d46264c1dc06d3d5
jonanv <jonan-vargas23@hotmail.com>
2023-12-12 08:04:11 -0500

    :art: (challenge 05): Add challenge 05 resolve

--- 2023/challenge05/index.js
+++ 2023/challenge05/index.js
@@ -8,25 +8,28 @@ let invalidUsers = [];
 const invalidUsersFirstLetters = [];
 
 let regex = {
-    id: /^[a-zA-Z0-9]+$/,
-    username: /^[a-zA-Z0-9]+$/,
-    email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-    age: /^[0-9]+$/,
-    location: /^[a-zA-Z]+$/
+    id: /^[a-z0-9]+$/i,
+    username: /^[a-z0-9]+$/i,
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    age: /^\d+$/,
+    location: /^\w+(\s+\w+)*$/,
 };
 
 arrayData.forEach((user) => {
-    if (validateUser(user)) {
-        validUsers.push(user);
-    } else {
+    const [ id, username, email, age, location ] = user.split(',');
+    if (!validateUser({ id, username, email, age, location })) {
         invalidUsers.push(user);
+        invalidUsersFirstLetters.push(username.split('')[0]);
+    } else {
+        validUsers.push(user);
     }
 });
 
 console.log(invalidUsers);
+console.log(`submit ${ invalidUsersFirstLetters.join('') }`);
 
 function validateUser(user) {
-    let [ id, username, email, age, location ] = user.split(',');
+    let { id, username, email, age, location } = user;
     return (
         id.length > 0 &&
         regex['id'].test(id) &&
@@ -38,11 +41,3 @@ function validateUser(user) {
         (location.length === 0 || regex['location'].test(location))
     );
 }
-
-// user.forEach((attr, index) => {
-//     if (attr.length > 0 && regex[index].test(attr)) {
-//         console.log(true);
-//     }
-// });
-// console.log(regex[2].test('jonan.vargas23@hotmail.com'));
-
